8c83aecf41a49edc14bdd3947a4a414e0642e369
Mike Smart <mike.smart@jadu.co.uk>
2018-03-31 13:36:04 +0100

    comments

--- src/reducers/player.js
+++ src/reducers/player.js
@@ -27,28 +27,40 @@ export default function (state = playerState, action) {
 
         case FILL_RECEPTACLE: {
             const { receptacle, measure } = action.payload;
+
+            // Get index of player receptacle
             const index = state.get('inventory').findIndex(item =>
                 item.get('uid') === receptacle.get('uid'));
             const contents = state.getIn(['inventory', index]).get('contents');
+
+            // Get the capacity of the receptacle
             const capacity = receptacle.getIn(['volume', 'amount']);
+
+            // Get the current occupied volume of the receptacle
             const volume = contents.reduce((v, c) => v += c.get('amount'), 0);
 
+            // Get the existing contents object if there is a measure of
+            // the same substance inside the receptacle
             const existingContentIndex = contents.findIndex(content => {
                 return content.get('substance') === measure.get('substance');
             });
 
+            // Add the contents if the substance is not already
+            // inside the receptacle
             if (existingContentIndex === -1) {
                 return state.updateIn(['inventory', index], item => {
                     return item.update('contents', c => c.push(measure))
                 });
             }
 
-            if (volume + measure.get('amount') >= capacity) {
-                return state.updateIn(['inventory', index], item =>
-                    item.updateIn(['contents', existingContentIndex], c =>
-                        c.update('amount', a => capacity)));
+            // If the next measure will exceed the capacity if the receptacle
+            // let's throw an error. We need to top up the receptacle and spill
+            // the rest...
+            if (volume >= capacity) {
+                throw new Error(`Receptacle "${receptacle.get('uid')}" is full!`);
             }
 
+            // Update the receptacle contents
             return state.updateIn(['inventory', index], item =>
                 item.updateIn(['contents', existingContentIndex], c =>
                     c.update('amount', a =>
